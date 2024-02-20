9ddcf3aa13a45ec619ad8c8278f37628dde6bd7f
Yash Kadve <57342703+Yashkadve@users.noreply.github.com>
2024-02-20 05:54:30 +0000

    Objects pushed

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,16 +1,38 @@
-const newObject={}
+// singleton
+// Object.create
 
-newObject.id="abc123"
-newObject.name="Yash"
-newObject.isLoggedIn = false
-// console.log(newObject);
+// object literals
 
-const regularUser={
-    email:"abc@gov.in",
-    fullname:{
-        userfullname:{
-            firstname:"Luci",
-            lastname:"medelo"
-        }
-    }
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
 }
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
