a301f27fa190a6c9e1dad5addd136403026588af
Yash Kadve <57342703+Yashkadve@users.noreply.github.com>
2024-02-20 05:07:40 +0000

    Array & obejct added

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+const newObject={}
+
+newObject.id="abc123"
+newObject.name="Yash"
+newObject.isLoggedIn = false
+// console.log(newObject);
+
+const regularUser={
+    email:"abc@gov.in",
+    fullname:{
+        userfullname:{
+            firstname:"Luci",
+            lastname:"medelo"
+        }
+    }
+}
